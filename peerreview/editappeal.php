b771e6fc276dd4654ba57025032a33ef4c5e3014
Chris Thornton <cwthornt@cs.ubc.ca>
2012-11-13 14:16:28 -0800

    Fixed a bug that stopped people from seeing their appeals

--- peerreview/editappeal.php
+++ peerreview/editappeal.php
@@ -57,7 +57,7 @@ try
         }
 
         //If we're after the stop date, we better be sure that this appeal exists
-        if($assignment->appealStopDate < $NOW && !$assignment->appealExists($review->matchID))
+        if($assignment->appealStopDate < $NOW && !$assignment->appealExists($review->matchID, $appealType))
         {
             $content .= "Appeal submissions are closed";
             render_page();

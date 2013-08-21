274bdf68c394748beba61e51a58e794e64fe5de8
CS 430 course account <cs430@kunghit.ugrad.cs.ubc.ca>
2013-08-21 12:59:37 -0700

    Maybe permissions will actually stick now

--- fixpermissions.sh
+++ fixpermissions.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-find ./ -name "*.png" -not -path "./.git*" -exec chmod +r {} \;
-find ./ -name "*.css"  -not -path "./.git*"-exec chmod +r {} \;
-find ./ -name ".user.ini" -not -path "./.git*" -exec chmod +r {} \;
-find ./ -name ".htaccess" -not -path "./.git*" -exec chmod +r {} \;
-find ./ -name ".htpasswd" -not -path "./.git*" -exec chmod +r {} \;
+find ./ -name "*.png" -not -path "./.git*" -exec chmod a+r {} \;
+find ./ -name "*.jpg" -not -path "./.git*" -exec chmod a+r {} \;
+find ./ -name "*.css"  -not -path "./.git*" -exec chmod a+r {} \;
+find ./ -name ".user.ini" -not -path "./.git*" -exec chmod a+r {} \;
+find ./ -name ".htaccess" -not -path "./.git*" -exec chmod a+r {} \;
+find ./ -name ".htpasswd" -not -path "./.git*" -exec chmod a+r {} \;
 find ./ -name "*.php" -not -path "./.git*" -exec chmod a+rx {} \;
 find ./ -name "*.js" -not -path "./.git*" -exec chmod a+rx {} \;
 find ./ -type d -not -path "./.git*" -exec chmod a+rx {} \;

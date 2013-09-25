de7b6b37932ff2510587744a55505568f19bf8c8
James Wright <james@chumsley.org>
2013-09-25 13:52:30 -0700

    Dumps actually live in the homedir, not the mta root

--- fabfile.py
+++ fabfile.py
@@ -10,18 +10,13 @@ def load_dump():
     with lcd(os.path.dirname(os.path.abspath(env.real_fabfile))):
         if not os.path.exists(env.lcwd + "/" + mysql_dumpfile):
             dump_mysql()
-            copy_dump()
+            get(mysql_dumpfile, mysql_dumpfile)
         local("mysql --user=mta mta --password=mta < %s" % mysql_dumpfile)
         local("mysql --user=mta mta --password=mta < local.sql")
 
 @task
 def dump_mysql(force = False):
-    with cd('public_html/mta'):
-        if force:
-            run('mysqldump --socket ~/mysql/tmp/mysql.sock.cs430 -u root -p mta > %s' % mysql_dumpfile)
-        else:
-            run('if [ ! -e %s ]; then mysqldump --socket ~/mysql/tmp/mysql.sock.cs430 -u root -p mta > %s; fi' % (mysql_dumpfile, mysql_dumpfile))
-
-def copy_dump():
-    with cd('public_html/mta'):
-        get(mysql_dumpfile, env.lcwd + "/" + mysql_dumpfile)
+    if force:
+        run('mysqldump --socket ~/mysql/tmp/mysql.sock.cs430 -u root -p mta > %s' % mysql_dumpfile)
+    else:
+        run('if [ ! -e %s ]; then mysqldump --socket ~/mysql/tmp/mysql.sock.cs430 -u root -p mta > %s; fi' % (mysql_dumpfile, mysql_dumpfile))

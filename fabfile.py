dda3f593f744768e042a08a2ff143760eb444b42
James Wright <james@chumsley.org>
2013-09-25 13:48:53 -0700

    Add fabric tasks for managing sql dumps

---
+++ fabfile.py
@@ -0,0 +1,27 @@
+import os.path
+import datetime
+from fabric.api import *
+
+env.hosts = ['cs430@kunghit.ugrad.cs.ubc.ca']
+mysql_dumpfile = datetime.date.today().strftime('mtadump-%Y%b%d.sql').lower()
+
+@task
+def load_dump():
+    with lcd(os.path.dirname(os.path.abspath(env.real_fabfile))):
+        if not os.path.exists(env.lcwd + "/" + mysql_dumpfile):
+            dump_mysql()
+            copy_dump()
+        local("mysql --user=mta mta --password=mta < %s" % mysql_dumpfile)
+        local("mysql --user=mta mta --password=mta < local.sql")
+
+@task
+def dump_mysql(force = False):
+    with cd('public_html/mta'):
+        if force:
+            run('mysqldump --socket ~/mysql/tmp/mysql.sock.cs430 -u root -p mta > %s' % mysql_dumpfile)
+        else:
+            run('if [ ! -e %s ]; then mysqldump --socket ~/mysql/tmp/mysql.sock.cs430 -u root -p mta > %s; fi' % (mysql_dumpfile, mysql_dumpfile))
+
+def copy_dump():
+    with cd('public_html/mta'):
+        get(mysql_dumpfile, env.lcwd + "/" + mysql_dumpfile)

97929244a974e120293fb30cbdf7ad4a14adb7e7
MiguelGamis <mgl_gms@hotmail.com>
2014-07-30 16:59:48 -0700

    working on dynamic assignment output on select of offering

--- scripts/copyassignments.php
+++ scripts/copyassignments.php
@@ -1,6 +1,6 @@
 <?php
 
-class CopyAssignments extends Script
+class CopyAssignmentsScript extends Script
 {
 	function getName()
     {
@@ -12,12 +12,45 @@ class CopyAssignments extends Script
     }
     function getFormHTML()
     {
-        return "(None)";
+    	global $dataMgr;
+		
+    	$html = "";
+		$html .= "<select name='courseSelect' id='courseSelect'>";
+		foreach($dataMgr->getCourses() as $courseObj){
+			$html .= "<option value='$courseObj->name'>$courseObj->displayName</option>";
+		}
+		$html .= "</select><br>	";
+		
+		$html .= "<script type='text/javascript'>
+        $('#courseSelect').change(function(){
+            $('#' + this.value).show().siblings().hide();
+        });
+        $('#courseSelect').change();
+        </script>\n";
+		
+		$html .= "<select name='sometext' size='5'>
+		  <option>text1</option>
+		  <option>text2</option>
+		  <option>text3</option>
+		  <option>text4</option>
+		  <option>text5</option>
+		</select>";
+		
+        return $html;
     }
     function hasParams()
     {
-        return false;
+        return true;
     }
+
+	function executeAndGetResult()
+	{
+		
+			
+			
+		return;
+	}
+
 } 
 
 ?>

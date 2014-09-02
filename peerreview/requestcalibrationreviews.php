a58813d0d4b00b9004af7f64c345a61a5dc79998
Miguel Gamis <mglgms@alvand.cs.ubc.ca>
2014-09-02 16:54:31 -0700

    starting

--- peerreview/requestcalibrationreviews.php
+++ peerreview/requestcalibrationreviews.php
@@ -7,19 +7,26 @@ try
     $authMgr->enforceLoggedIn();
 
     $assignment = get_peerreview_assignment();
-
-    $submissionID = $assignment->getNewCalibrationSubmissionForUser($USERID);
-
-    if(!is_null($submissionID))
-    {
-        $assignment->assignCalibrationReview($submissionID, $USERID); 
-        //redirect_to_main();
-        $newID = sizeof($assignment->getAssignedCalibrationReviews($USERID)) - 1;
-        redirect_to_page("peerreview/editreview.php?assignmentid=$assignment->assignmentID&calibration=$newID");
-    }
-
-    //If we don't escape, say that they've done everything
-    $content .= "<h3>Oops</h3>There are no more calibration essays to do!";
+	
+	if($assignment->submissionSettings->autoAssignEssayTopic == true)
+	{
+		$USERstudentID = $dataMgr->getUserInfo($USERID)->studentID;	
+	}
+	else 
+	{
+	    $submissionID = $assignment->getNewCalibrationSubmissionForUser($USERID);
+	
+	    if(!is_null($submissionID))
+	    {
+	        $assignment->assignCalibrationReview($submissionID, $USERID); 
+	        //redirect_to_main();
+	        $newID = sizeof($assignment->getAssignedCalibrationReviews($USERID)) - 1;
+	        redirect_to_page("peerreview/editreview.php?assignmentid=$assignment->assignmentID&calibration=$newID");
+	    }
+	
+	    //If we don't escape, say that they've done everything
+	    $content .= "<h3>Oops</h3>There are no more calibration essays to do!";
+	}
     render_page();
 
 }catch(Exception $e){

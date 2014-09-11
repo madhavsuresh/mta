7ecf50ad5c9ef882f1c74584004f523f70252441
Miguel Gamis <mglgms@alvand.cs.ubc.ca>
2014-09-11 15:42:32 -0700

    done, but missing confirmation of student review being marked in multi-form page

--- tasks_TA.php
+++ tasks_TA.php
@@ -16,22 +16,51 @@ foreach($assignments as $assignment)
 	$spotChecks = $assignment->getSpotChecksForMarker($USERID);
 	$reviewMap = $assignment->getReviewMap();
 	
+	$color = '';
+	if($NOW >= $assignment->markPostDate)
+		$color = 'red'; 
+	
+	//For each of the marker's assigned reviews
 	foreach($reviews as $reviewObj)
 	{
-		if($reviewObj->exists)
+		$studentReviews = $assignment->getReviewsForSubmission($reviewObj->submissionID);
+		$allReviewsMarked = true;
+		$numStudentReviews = 0;
+		$numUnmarkedStudentReviews = 0;
+		foreach($studentReviews as $studentReview)
 		{
-			
-		}	
-		else 
+			if(!$dataMgr->isStudent($studentReview->reviewerID))
+				continue;
+			if(!($reviewMap[$reviewObj->submissionID->id][$studentReview->reviewerID->id]->instructorForced))
+			{
+				if(!$assignment->getReviewMark($studentReview->matchID)->isValid)
+				{
+					$numUnmarkedStudentReviews++;
+				}
+				$numStudentReviews++;
+			}
+		}
+		$allReviewsMarked = ($numUnmarkedStudentReviews == 0);
+		$numMarkedStudentReviews = $numStudentReviews - $numUnmarkedStudentReviews;
+		
+		if(!$reviewObj->exists || !$allReviewsMarked)
 		{
+			$markerReviewStatus = "Not Done";
+			if($reviewObj->exists)
+				$markerReviewStatus = "Done";
+			if($assignment->reviewDraftExists($reviewObj->matchID))
+				$markerReviewStatus = "Draft only";
+				
 			$reviewTask = new stdClass();
+			$reviewTask->endDate = $assignment->markPostDate;
 			$reviewTask->html = 
 			"<table width='100%'><tr><td class='column1'><h4>$assignment->name</h4></td>
 			<td class='column2'>Review</td>
-			<td class='column3'><a target='_blank' title='Edit' href='".get_redirect_url("peerreview/editreview.php?assignmentid=$assignment->assignmentID&submissionid=$reviewObj->submissionID&matchid=$reviewObj->matchID&close=1")."'><button>Go</button></a></td>
-			<td class='column4'>".phpDate($assignment->markPostDate)."</td></tr></table>\n";
+			<td class='column3'><table><td>Your Review: $markerReviewStatus<br>$numMarkedStudentReviews of $numStudentReviews reviews are marked </td><td><a target='_blank' title='Edit' href='".get_redirect_url("peerreview/editreview.php?assignmentid=$assignment->assignmentID&submissionid=$reviewObj->submissionID&matchid=$reviewObj->matchID&close=1&showall=1")."'><button>Grade</button></a></td></table></td>
+			<td class='column4'><span style='color:$color'>".phpDate($assignment->markPostDate)."</span></td></tr></table>\n";
 			insert($reviewTask, $reviewTasks);
-		}	
+		}
+		
 	}
 	
 	foreach($spotChecks as $spotCheck)
@@ -51,12 +80,14 @@ foreach($assignments as $assignment)
                     }
                 }
             }
+			
             $reviewTask = new stdClass();
+			$reviewTask->endDate = $assignment->markPostDate;
 			$reviewTask->html = 
 			"<table width='100%'><tr><td class='column1'><h4>$assignment->name</h4></td>
 			<td class='column2'>Spot Check</td>
             <td><a  target='_blank' href='peerreview/viewer.php?assignmentid=$assignment->assignmentID&$args&type$i=spotcheck&submissionid$i=$spotCheck->submissionID'><button>Go</button><br></a></td>
-            <td class='column4'>".phpDate($assignment->markPostDate)."</td></tr></table>\n";
+            <td class='column4'><span style='color:$color'>".phpDate($assignment->markPostDate)."</span></td></tr></table>\n";
 			insert($reviewTask, $reviewTasks);
         }
 	}

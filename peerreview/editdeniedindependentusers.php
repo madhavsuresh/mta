f583493781639414b824bd0272ebc2c30078f0a8
Miguel Gamis <mglgms@alvand.cs.ubc.ca>
2014-10-03 11:17:00 -0700

    select all as independent achieved through javascript

--- peerreview/editdeniedindependentusers.php
+++ peerreview/editdeniedindependentusers.php
@@ -37,8 +37,9 @@ try
         $deniedUsers = $assignment->getDeniedUsers();
         $independentUsers = $assignment->getIndependentUsers();
 
-		$independentMap = array();
+		$independentsRecord = array();
         $currentRowType = 0;
+		$i = 0;
         foreach($dataMgr->getUserDisplayMap() as $user => $name ){
             if(!$dataMgr->isStudent(new UserID($user)))
                 continue;
@@ -50,31 +51,39 @@ try
             if(array_key_exists($user, $independentUsers))
 			{
                 $independentChecked = 'checked';
-				$independentMap[$user] = 1;
+				$independentsRecord[$i] = 1;
 			}
 			else
-				$independentMap[$user] = 0;
-            $content .= "<tr class='rowType$currentRowType'><td>$name</td><td><input type='checkbox' name='denied[]' value='$user' $deniedChecked /> Denied </td><td><input type='checkbox' name='independent[]' class='independent' value='$user' $independentChecked /> Independent </td></tr>\n";
+				$independentsRecord[$i] = 0;
+            $content .= "<tr class='rowType$currentRowType'><td>$name</td><td><input type='checkbox' name='denied[]' value='$user' $deniedChecked /> Denied </td><td><input type='checkbox' name='independent[]' id='independent$i' class='independents' value='$user' $independentChecked /> Independent </td></tr>\n";
             $currentRowType = ($currentRowType+1)%2;
+			$i++;
         }
         $content .= "<tr><td>&nbsp;</td><td>\n";
-		$content .= "<tr><td></td><td></td><td><input type='checkbox' name='allIndependent' id='allIndependent'/>All</td></tr>";
+		$content .= "<tr><td></td><td></td><td><input type='checkbox' name='allIndependent' id='allIndependent'/>Select All</td></tr>";
         $content .= "</table>\n";
         $content .= "<br><input type='submit' value='Save' />\n";
         $content .= "</form>\n";
 
 		$content .= "<script type='text/javascript'>";
 		$content .= "var independent = new Array();";
-		foreach($independentMap as $user => $isIndependent)
-			$content .= "independent[$user] = $isIndependent;";
-		$content .= "</script>";
-	
-		$content .= "<script type='text/javascript'>		
-        $('#allIndependent').change(function(){
+		$i = 0;
+		foreach($independentsRecord as $isIndependent)
+		{
+			$content .= "independent[$i] = $isIndependent;";
+			$i++;
+		}
+        $content .= "$('#allIndependent').change(function(){
 			if(this.checked){
-				$('.independent').prop('checked', true);
+				$('.independents').prop('checked', true);
 			}else{
-				$('.independent').prop('checked', false);
+				for(i = 0, len = independent.length; i < len; i++)
+				{
+					if(independent[i]==1)
+						$('#independent' + i.toString()).prop('checked', true);
+					else
+						$('#independent' + i.toString()).prop('checked', false);	
+				}
 			}
         });
         $('#courseSelect').change();

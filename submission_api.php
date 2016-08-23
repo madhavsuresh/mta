11e9a0eab00fa60be82a74e70728b34896f54acb
William Wallace <william@enron.cs.northwestern.edu>
2016-08-23 11:33:55 -0500

    added sql query to populate the essays table

--- submission_api.php
+++ submission_api.php
@@ -3,43 +3,43 @@ require_once("inc/common.php");
 require_once("inc/datamanager.php");
 require_once("peerreview/inc/datamanagers/pdoassignmentdatamanager.php");
 require_once("config.php");
-function populate_submissions($assignment_id, $text){
-    echo "i got called";
-    return 0;
-}
 
-echo "hello world";
+
+$text_to_add = "The ouick brown fox jumped over the fence";
+$DELETE_FLAG = TRUE;
+try{
+
 $db = $dataMgr->getDatabase();
-#$db = new PDO($MTA_DATAMANGER_PDO_CONFIG["dsn"], $MTA_DATAMANAGER_PDO_CONFIG["username"],
-                              #                  $MTA_DATAMANAGER_PDO_CONFIG["password"],
-                               #                 array(PDO::ATTR_PERSISTENT => true));
-$counter = 0;
-$sh = $db->prepare("SELECT * FROM users; ");
-$sh->execute();
-$size = $sh->fetchall();
-$size = count($size);
-print_r($size);
+$db->beginTransaction();
+
+if ($DELETE_FLAG == TRUE )
+    {
+        $sh = $db->prepare("DELETE FROM peer_review_assignment_submissions;");
+        $sh->execute();
+        $sh = $db->prepare("DELETE FROM peer_review_assignment_essays;");
+        $sh->execute();
+
+    }
+
 $sh = $db->prepare("SELECT userID FROM users WHERE userType = 'student';");
 $sh->execute();
 $students = $sh->fetchall();
-print_r($students);
 
 foreach ($students as $student){
     $sh= $db->prepare("Insert INTO peer_review_assignment_submissions (assignmentID, authorID, noPublicUse, submissionTimestamp) Values(?,?,?, ".$dataMgr->from_unixtime("?").");");
     $sh->execute(array(1,$student->userID, 0, $NOW ));
-    echo $student->userID;
+    #^ the first number is assignment id for now hardcoded.
+    $lastID = $db->lastInsertId();
 
+    $sh = $db->prepare("Insert INTO peer_review_assignment_essays  Values(?,?,?);");
+    $sh->execute(array($lastID, $text_to_add, NULL));
 }
 
+$db->commit();
 
-
-
-$sh = $db->prepare("INSERT INTO peer_review_assignment_submissions (assignmentID, authorID, noPublicUse, submissionTimestamp) VALUES(?,?,?,?)");
-# echo $db;
-
-
-
-
+} catch (Exception $e){
+   echo $e->getMessage();
+}
 
 ?>
 

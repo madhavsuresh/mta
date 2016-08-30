038acf25521d08153ab2815790c9db442ff83ddd
Madhav Suresh <madhav.a.suresh@gmail.com>
2016-08-30 13:30:58 -0500

    intermediate commit

--- api/index.php
+++ api/index.php
@@ -3,14 +3,23 @@ require '../vendor/autoload.php';
 require_once("../inc/common.php");
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use \JsonSchema\Validator as JsonValidator;
 
 $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
-$app = new \Slim\App(["settings" => $config]);
+$container = new \Slim\Container;
+/*
+$container['schemaArray'] =  function() {
+	$schemaArray = [];
+//	$schemaArray['/peermatch/get'] = decode_json_throw_errors(file_get_contents('schemas/peermatch/get.schema'));
+	return $schemaArray;
+};
+ */
+//$app = new \Slim\App($container,["settings" => $config]);
+$app = new \Slim\App(["settings"=>$config]);
 
-$app->get('/', function (Request $request, Response $response) {
-	$response->getBody()->write("Hello, $rootUri");
-});
+
+
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
@@ -18,17 +27,62 @@ $app->get('/hello/{name}', function (Request $request, Response $response) {
     return $response;
 });
 
-$app->get('/getallstudents/{course}', function (Request $request, Response $response) use ($dataMgr) {
-	$dataMgr->setCourseFromName($request->getAttribute('course'));
-	$students = $dataMgr->getStudents();
-	$newResponse = $response->withJson($students);
-	return $newResponse;
+function decode_json_throw_errors($inputString) {
+	$json_body = json_decode($inputString);
+	switch (json_last_error()) {
+	case JSON_ERROR_NONE:
+		break;
+	case JSON_ERROR_DEPTH:
+		throw new Exception('JSON Maximum stack depth exceeded');
+		break;
+	case JSON_ERROR_STATE_MISMATCH:
+		throw new Exception('Underflow or the modes mismatch');
+		break;
+	case JSON_ERROR_CTRL_CHAR:
+		throw new Exception('Unexpected control character found');
+		break;
+	case JSON_ERROR_SYNTAX:
+		throw new Exception('Syntax error, malformed JSON');
+		break;
+	case JSON_ERROR_UTF8:
+		throw new Exception('Malformed UTF-8 characters, possibly incorrectly encoded');
+		break;
+	default:
+		throw new Exception('Unknown Error');
+		break;
+	}
+	return $json_body;
+}
+
+$jsonDecodeMW = function ($request, $response, $next) {
+	$json_body = decode_json_throw_errors($request->getBody());
+	$request = $request->withAttribute('requestDecodedJson', $json_body);
+	$response = $next($request, $response);
+	return $response;	
+};
+
+$jsonvalidatemw = function ($request, $response, $next) {
+	$json_body = json_decode($request->getBody());
+	$validator = new JsonValidator;
+	$validator->check($data, (object)['$ref' => 'file://' . realpath('./peermatch_get.json')]);
+};
+
+$app->post('/validate', function (Request $request, Response $response) {
+	$json_body = $request->getAttribute('requestDecodedJson');
+	$schema = json_decode(file_get_contents('./peermatch/get/request.json'));
+	$validator = new League\JsonGuard\Validator($json_body, $schema);
+	if ($validator->fails()) {
+		print_r($validator->errors());
+	} else {
+		echo "not";
+	}
+	//$schemaArray = $myService->schemaArray;
+})->add($jsonDecodeMW);
+
+$app->get('/peermatch/create', function (Request $request, Response $response) {
+
 });
 
-$app->get('/getallsubmissions/{course}/{assignmentID}', function (Request $request, Response $response) use ($dataMgr) {
-	$dataMgr->setCourseFromName($request->getAttribute('course'));
-	
-});
 
 //TODO: proper exception handling. 
 $app->post('/uploadpeermatch/{course}/{assignmentID}', function (Request $request, Response $response) use ($dataMgr) {
